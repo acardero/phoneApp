4e0c56a296ebbb49dd22be7f4f323f9e6161f22a
Alan Cardero <alan.cardero@gmail.com>
2018-08-18 13:48:32 -0400

    added upload component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { routes } from './app.routing';
 import { HttpModule } from '@angular/http';
+import { FileUploadModule } from "ng2-file-upload";
+import { FormsModule } from "@angular/forms";
+
 
 import { PhoneService} from './services/phone.service';
 import { AppComponent } from './app.component';
 import { PhoneListComponent } from './phone-list/phone-list.component';
 import { PhoneDetailsComponent } from './phone-details/phone-details.component';
+import { AddPhoneComponent } from './add-phone/add-phone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhoneListComponent,
-    PhoneDetailsComponent
+    PhoneDetailsComponent,
+    AddPhoneComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     HttpModule,
+    FormsModule,
+    FileUploadModule
   ],
   providers: [PhoneService],
   bootstrap: [AppComponent]
